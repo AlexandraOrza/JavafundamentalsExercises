520d969557e8f69b139adba21f53c8f41acd2130
Alexandra Orza <aralexandracornelia@gmail.com>
2021-01-30 22:59:48 +0200

    StringExercises

--- src/main/java/fundamentals/StringExercises.java
+++ src/main/java/fundamentals/StringExercises.java
@@ -2,7 +2,7 @@ package fundamentals;
 
 public class StringExercises {
     public static void repeatText(){
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 3; i++){
             System.out.println("I love this time of the year");
         }
     }
